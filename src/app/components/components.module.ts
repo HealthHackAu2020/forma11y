0c10f0d8b3997bdded51359a007a53cef98d85da
Anas Fares <afar44@student.monash.edu>
2020-07-28 03:14:57 +1000

    Revert "Merge branch 'master' of https://github.com/HealthHackAu2020/forma11y"
    This reverts commit e6afe99b6dfbbeff0a0d5863f462c522b2a09040, reversing
    changes made to e0b97cd02eb299154e15a2c6015ad8289eb96d24.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from "@angular/core";
 import { IonicModule } from '@ionic/angular';
 import { ValidationErrorComponent } from './validation-error/validation-error.component';
 import { CommonModule } from '@angular/common';
-import { AccessibilityFeaturesComponent } from './accessibility-features/accessibility-features.component';
 @NgModule({
-      declarations: [ValidationErrorComponent, AccessibilityFeaturesComponent],
-      exports: [ ValidationErrorComponent, AccessibilityFeaturesComponent],
+      declarations: [ValidationErrorComponent],
+      exports: [ ValidationErrorComponent],
       imports: [IonicModule, CommonModule]
   })
   export class ComponentsModule {}
--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ValidationService } from '../validation.service';
-import { PopoverController } from '@ionic/angular';
-import { AccessibilityFeaturesComponent } from '../components/accessibility-features/accessibility-features.component';
 
 @Component({
   selector: 'app-form',
@@ -13,7 +11,7 @@ export class FormPage implements OnInit {
   title: string = 'Form Elements';
   dummyForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder, private popoverController: PopoverController) {
+  constructor(public formBuilder: FormBuilder) {
     this.dummyForm = this.formBuilder.group({
       input1: ['', ValidationService.isRequired],
       input2: ['', ValidationService.isRequired],
@@ -21,13 +19,8 @@ export class FormPage implements OnInit {
     });
   }
 
-  ngOnInit() {}
-
-  async showA11yFeatures(event: any) {
-    const popover = await this.popoverController.create({
-      component: AccessibilityFeaturesComponent,
-      event
-    })
-    return await popover.present();  
+  ngOnInit() {
   }
+
+
 }

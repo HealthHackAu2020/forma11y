95ca20116717ed8d9f3d1a67118b27b20560064d
Neil <neilbo21@gmail.com>
2020-07-28 15:38:32 +1000

    Add reactive forms module; shared components module

--- src/app/new-patient-registration/new-patient-registration.module.ts
+++ src/app/new-patient-registration/new-patient-registration.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { NewPatientRegistrationPageRoutingModule } from './new-patient-registration-routing.module';
 
 import { NewPatientRegistrationPage } from './new-patient-registration.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NewPatientRegistrationPageRoutingModule
+    NewPatientRegistrationPageRoutingModule,
+    ReactiveFormsModule,
+    ComponentsModule
   ],
   declarations: [NewPatientRegistrationPage]
 })

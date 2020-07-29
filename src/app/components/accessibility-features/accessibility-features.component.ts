7e7df2022f31e2f5d5009762b90660145f913d9a
Neil <neilbo21@gmail.com>
2020-07-30 01:08:17 +1000

    WIP :: Tryinng to get reset button to work.

--- src/app/components/accessibility-features/accessibility-features.component.ts
+++ src/app/components/accessibility-features/accessibility-features.component.ts
@@ -3,21 +3,32 @@ import { DarkModeService } from 'src/app/dark-mode.service';
 import { FontSizeService } from 'src/app/font-size.service';
 import { ReaderModeService } from 'src/app/reader-mode.service';
 
+export interface A11YFeatures {
+  isDarkMode: boolean,
+  fontSize: number,
+  isReaderMode: boolean,
+}
+
 @Component({
   selector: 'app-accessibility-features',
   templateUrl: './accessibility-features.component.html',
   styleUrls: ['./accessibility-features.component.scss'],
 })
+
 export class AccessibilityFeaturesComponent implements OnInit {
-  isDarkMode: boolean;
-  fontSize: number;
-  isReaderMode: boolean;
+  a11yFeatures: A11YFeatures;
+  
   constructor(private darkModeService: DarkModeService, private fontSizeService: FontSizeService, private readerModeService: ReaderModeService) {}
 
   ngOnInit() {
-    this.isDarkMode = this.darkModeService.get();
-    this.fontSize = this.fontSizeService.get();
-    this.isReaderMode = this.readerModeService.get();
+    this.a11yFeatures = {
+      isDarkMode: this.darkModeService.get(),
+      fontSize: this.fontSizeService.get(),
+      isReaderMode: this.readerModeService.get(),  
+    }
+    // this.isDarkMode = this.darkModeService.get();
+    // this.fontSize = this.fontSizeService.get();
+    // this.isReaderMode = this.readerModeService.get();
   }
 
   async onToggleDarkMode(event: CustomEvent): Promise<void> {
@@ -29,7 +40,7 @@ export class AccessibilityFeaturesComponent implements OnInit {
       this.darkModeService.set(false);
       document.body.setAttribute('color-theme', 'light');
     }
-    console.info(this.isDarkMode);
+    console.info(this.a11yFeatures.isDarkMode);
   }
   
   async onChangeFontSize(event: any) {
@@ -46,7 +57,16 @@ export class AccessibilityFeaturesComponent implements OnInit {
       this.readerModeService.set(false);
       document.body.setAttribute('font-readable', 'false');
     }
-    console.info(this.isReaderMode);
+    console.info(this.a11yFeatures.isReaderMode);
+  }
+
+  restoreDefaults() {
+    // WIP :: Not working
+    this.a11yFeatures = {
+      isDarkMode: false,
+      fontSize: 1.5,
+      isReaderMode: false,  
+    }
   }
 
 }

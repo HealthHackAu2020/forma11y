fa12a9b41194d555867138c625416ae1ad6c17ff
Neil <neilbo21@gmail.com>
2020-07-30 00:36:51 +1000

    Add a11y feature to homeepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FontSizeService } from '../font-size.service';
+import { DarkModeService } from '../dark-mode.service';
+import { PopoverController } from '@ionic/angular';
+import { AccessibilityFeaturesComponent } from '../components/accessibility-features/accessibility-features.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   title: string = 'Welcome';
-  constructor() { }
+  isDarkMode: boolean;
+  fontSize: number;
+  constructor(private popoverController: PopoverController, private darkModeService: DarkModeService, private fontSizeService: FontSizeService, private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.isDarkMode = this.darkModeService.get();
+    this.fontSize = this.fontSizeService.get();
   }
 
+  async showA11yFeatures(event: any) {
+    const popover = await this.popoverController.create({
+      component: AccessibilityFeaturesComponent,
+      event
+    })
+    return await popover.present();
+  }
+
+  ngAfterViewChecked() {
+    this.fontSize = this.fontSizeService.get();
+    this.changeDetectorRef.detectChanges(); // https://stackoverflow.com/questions/34364880/expression-has-changed-after-it-was-checked
+  }
 }

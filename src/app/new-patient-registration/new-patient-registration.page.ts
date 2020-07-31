6ea99a4d6c1608939f5f10923baccc5cecdc7c01
Neil <neilbo21@gmail.com>
2020-07-31 12:40:31 +1000

    Medicare fform validation

--- src/app/new-patient-registration/new-patient-registration.page.ts
+++ src/app/new-patient-registration/new-patient-registration.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DarkModeService } from '../dark-mode.service';
-import { PopoverController } from '@ionic/angular';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { FontSizeService } from '../font-size.service';
+import { ValidationService } from '../validation.service';
 
 @Component({
   selector: 'app-new-patient-registration',
@@ -12,16 +12,14 @@ import { FontSizeService } from '../font-size.service';
 export class NewPatientRegistrationPage implements OnInit {
   title: string = 'New Patient Registration';
   isDarkMode: boolean;
-  newPatientRegistrationForm: FormGroup;
+  medicareCardForm: FormGroup;
   fontSize: number;
-
-  constructor(private darkModeService: DarkModeService, private fontSizeService: FontSizeService, private changeDetectorRef: ChangeDetectorRef) {
-    // TODO :: apply formBuilder to form in html file
-    // this.newPatientRegistrationForm = this.formBuilder.group({
-    //   input1: ['', ValidationService.isRequired],
-    //   input2: ['', ValidationService.isRequired],
-    //   email: ['', [ValidationService.isRequired, ValidationService.isEmailValid]],
-    // });
+  constructor(private darkModeService: DarkModeService, private fontSizeService: FontSizeService, private changeDetectorRef: ChangeDetectorRef, private formBuilder: FormBuilder) {
+    this.medicareCardForm = this.formBuilder.group({
+      cardNumber: new FormControl('', [ValidationService.isRequired, ValidationService.isValidMedicareCardNumber]),
+      cardRef: new FormControl('', [ValidationService.isRequired, ValidationService.isWholeNumber,ValidationService.isGreaterThan(0)]),
+      expiryDate: new FormControl('', [ValidationService.isRequired]),
+    });
   }
 
   ngOnInit() {
--- src/app/validation.service.ts
+++ src/app/validation.service.ts
@@ -1,4 +1,6 @@
-import * as _ from "lodash";
+import isEmpty from "lodash/isEmpty";
+import isString from "lodash/isString";
+import isNumber from "lodash/isNumber";
 import { AbstractControl } from '@angular/forms';
 
 export class ValidationService {
@@ -10,19 +12,20 @@ export class ValidationService {
   ): string {
     const config = {
       required: `${fieldName} is required`,
-      invalidEmailAddress: "Please enter a valid email address",
-      invalidPassword:
+      invalid_email: "Please enter a valid email address",
+      invalid_password:
         "Please enter a valid password. Password must be at least 6 characters long, and contain a number.",
       invalid_age: "Please enter a valid age",
-      invalidInches: "Please enter a valid height in inches",
+      invalid_medicare: "Please enter a valid Medicare card number",
+      whole_number: `${fieldName} must be a whole number`,
+      greater_than: `${fieldName} must be more than ${validatorValue}`,
     };
 
     return config[validatorName];
   }
 
   static isRequired(control: AbstractControl): { [key: string]: boolean } | null  {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
+    if (isEmpty(control.value)) {
       return { required: true };
     }
     return null;
@@ -33,7 +36,7 @@ export class ValidationService {
     if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
       return null;
     } else {
-      return { invalidEmailAddress: true };
+      return { invalid_email: true };
     }
   }
 
@@ -43,7 +46,7 @@ export class ValidationService {
     if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
       return null;
     } else {
-      return { invalidPassword: true };
+      return { invalid_password: true };
     }
   }
 
@@ -58,6 +61,63 @@ export class ValidationService {
     } else {
       return null;
     }
+  }
+
+  // Medicare: https://stackoverflow.com/questions/3589345/how-do-i-validate-an-australian-medicare-number
+  static isValidMedicareCardNumber(control: AbstractControl) {
+    const medicareNumber = control.value;
+    let isValid: boolean;
+
+    if (medicareNumber && medicareNumber.length === 10) {
+      const matches: boolean = medicareNumber.match(/^(\d{8})(\d)/);
+
+      if (!matches) {
+        return { invalid_medicare: true };
+      }
+
+      const base: string = matches[1];
+      const checkDigit: string = matches[2];
+      const weights: Array<number> = [1, 3, 7, 9, 1, 3, 7, 9];
+
+      let sum: number = 0;
+      for (let i = 0; i < weights.length; i++) {
+        sum += parseInt(base[i], 10) * weights[i];
+      }
+
+      isValid = sum % 10 === parseInt(checkDigit, 10);
+    }
+    return isValid ? null : { invalid_medicare: true };
+  }
+   
+  static WHOLE_NUMBER: RegExp = /^-?\d*(?:\.0+)?$/;
+
+  static isWholeNumber(control: AbstractControl) {
+    const val = control.value;
+    if (isString(val)) {
+      if (control.value.match(ValidationService.WHOLE_NUMBER)) {
+        return null;
+      } else {
+        return { whole_number : true };
+      }
+    } else if (isNumber(val)) {
+      if (Number.isInteger(val)) {
+        return null;
+      } else {
+        return { whole_number : true };
+      }
+    } else {
+      return { whole_number : true };
+    }
+  }
+
+  static isGreaterThan(limit: number) {
+    return (control: AbstractControl) => {
+      if (control.value > limit) {
+        return null;
+      }
+
+      return { greater_than: limit };
+    };
   }
 
 }
